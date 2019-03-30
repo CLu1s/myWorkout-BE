dd3e72a8755c4e69cf3db6d22d2cb68b8bd9da3b
Luis L <luis.luis@icloud.com>
2019-03-29 22:42:39 -0600

    feature(babel): add babel and header

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/api/v1/hello', (req, res) => {
+  res.send();
+  
+});
+app.get('/api/v1/hello', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    todos: { express: 'Hello From Express' }
+  })
 });
 
 app.post('/api/world', (req, res) => {
